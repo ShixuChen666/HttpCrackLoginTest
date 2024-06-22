86c08b08e7e244bae1c6bbcbe33e7c2fb5d8cbdb
the night <10220968+chen_shi_xu@user.noreply.gitee.com>
2024-06-23 02:01:10 +0800

    生成密码添加字符添加大写字母

--- HttpCrackLoginTest/passwordUtil.py
+++ HttpCrackLoginTest/passwordUtil.py
@@ -7,7 +7,9 @@ from autorequest import login
 def get_password():
     list1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
              'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+             'A','B',"C", 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     for i in range(3,10):
         combine(list1,i)
 
